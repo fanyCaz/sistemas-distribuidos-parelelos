91a2c9f43905f9db7226a6a0d78c879455c33725
Estefania <estefania.caceres@enevasys.com>
2020-09-19 20:13:07 -0500

    proyecto

---
+++ hilos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+namespace hilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger num = new BigInteger();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
