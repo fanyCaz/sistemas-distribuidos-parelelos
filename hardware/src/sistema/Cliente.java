d21453ed082035dc46eda34b0fde2d5cdc88ac62
Estefania <estefania.caceres@enevasys.com>
2020-11-09 15:43:38 -0600

    memoria en uso de computadora

--- hardware/src/sistema/Cliente.java
+++ hardware/src/sistema/Cliente.java
@@ -5,15 +5,19 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
 import oshi.hardware.HardwareAbstractionLayer;
-
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.CentralProcessor.TickType;
+import oshi.util.FormatUtil;
 
 import org.slf4j.Logger;
 
 public class Cliente {
  public static void main(String args[]) throws Exception {
-	 Index.index();
-	/*ObjectOutputStream oos = null;
+	 //Index.index();
+	ObjectOutputStream oos = null;
     ObjectInputStream ois = null;
     Socket s = null;
     SystemInfo si = new SystemInfo();
@@ -26,16 +30,33 @@ public class Cliente {
     String HDDStr = HDD.toString();
     String SO = System.getProperty("os.name");
 	        
+    //Uso Procesador
+    long[] prevTicks = new long[TickType.values().length];
+    double cpuLoad = hal.getProcessor().getSystemCpuLoadBetweenTicks(prevTicks) * 100;
+    prevTicks = hal.getProcessor().getSystemCpuLoadTicks();
+    System.out.println("load " + cpuLoad);
+    CentralProcessor processor = hal.getProcessor();
+    System.out.println("procesador \n" + processor.toString() + "\n termino-procesador"); 
+    //Memoria
+  //Memoria RAM
+    GlobalMemory gm = hal.getMemory();
+    long usedMemory = gm.getTotal() - gm.getAvailable();
+   
     String[] systemInfo = {
 	            "Modelo CPU: " + cpuModel, 
 	            "Frecuencia CPU: " + cpuFreqStr,
-	            "Memoria: " + memory, 
-	            "Almacenamiento: " + HDDStr, 
+	            "Memoria: " + memory,
+	            "Memoria En Uso " + FormatUtil.formatBytes(usedMemory),
+	            "Almacenamiento: " + HDDStr,
 	            "Sistema operativo: " + SO
             };
     
     try
     {
+    	for(String info: systemInfo) {
+    		System.out.println(info);
+    	}
+    	/*
 	    // instancio el server con la IP y el PORT
 	    s = new Socket("25.5.218.12",5432);
 	    oos = new ObjectOutputStream(s.getOutputStream());
@@ -44,7 +65,7 @@ public class Cliente {
 	    oos.writeObject(systemInfo);
 	    String ret = (String)ois.readObject();
         System.out.println( "Valor de servidor: " + ret);
-        
+        */
     }
     catch(Exception ex)
     {
@@ -55,6 +76,6 @@ public class Cliente {
 	    if( ois != null ) ois.close();
 	    if( oos != null ) oos.close();
 	    if( s != null ) s.close();
-    }*/
+    }
  }
 }
